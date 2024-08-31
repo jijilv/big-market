4eb6c117569495c0a0c8702ec983a78aeb3c4195
Phronima <1119819404@qq.com>
2024-08-31 12:55:52 +0800

    feat: 【营销服务】第13节：引入分库分表路由组件

---
+++ big-market-infrastructure/src/main/java/edu/szu/infrastructure/persistent/dao/IRaffleActivityOrderDao.java
@@ -0,0 +1,26 @@
+package edu.szu.infrastructure.persistent.dao;
+
+import cn.bugstack.middleware.db.router.annotation.DBRouter;
+import cn.bugstack.middleware.db.router.annotation.DBRouterStrategy;
+import edu.szu.infrastructure.persistent.po.RaffleActivityOrder;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 抽奖活动单Dao
+ * @create 2024-03-09 10:08
+ */
+@Mapper
+@DBRouterStrategy(splitTable = true)
+public interface IRaffleActivityOrderDao {
+
+    @DBRouter(key = "userId")
+    void insert(RaffleActivityOrder raffleActivityOrder);
+
+    @DBRouter
+    List<RaffleActivityOrder> queryRaffleActivityOrderByUserId(String userId);
+
+}
+
